56b62ce40695dd01357e472bb08554a96d1628d8
rikzun <rik.zunqq@gmail.com>
2021-11-19 02:03:15 +0300

    start rewrite to svelte

--- scripts/defaultSettings.js
+++
@@ -1,9 +0,0 @@
-const standardSettings = {
-    currentLang: 'en',
-    openClick: 'double'
-}
-
-for (const [key, value] of Object.entries(standardSettings)) {
-    if (localStorage.hasOwnProperty(key)) continue
-    localStorage.setItem(key, value)
-}
---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    {#if process.env.NODE_ENV !== 'dev123elopment'}
+        <h1>Production environment!</h1>
+    {/if}
+</template>
+
+<style lang="sass">
+    :global
+        body
+            display: flex
+            align-items: center
+            justify-content: center
+
+            margin: 0
+            width: 100vw
+            height: 100vh
+
+            background-image: url('https://palscity.ams3.cdn.digitaloceanspaces.com/upload/photos/2021/08/fVooSsgnWxS7uRJatGn7_19_b1ca2d4a591d79191715b66af54ddf3a_image.jpg?cache=0')
+            background-position: center
+            background-repeat: no-repeat
+            background-size: cover
+</style>
